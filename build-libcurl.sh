3276064e3ebddc510f1975de1c9b8f0d536716b7
ocvcoin <90601304+ocvcoin@users.noreply.github.com>
2023-01-30 22:10:04 +0000

    Update build-libcurl.sh

--- build-libcurl.sh
+++ build-libcurl.sh
@@ -7,6 +7,78 @@ export ANDROID_API_LEVEL="18"
 
 export TOOLCHAIN_VERSION="4.9"
 
+
+
+
+
+###############################################################################
+# Begin armv5te compile
+
+
+
+export ARCH_TARGET="armv5te-android-linux"
+export PLATFORM="arm-linux-androideabi"
+export ROOTDIR="/opt/android-ndk-r12b/arm/"
+export SYSROOT="${ROOTDIR}/sysroot"
+export DROIDTOOLS="${ROOTDIR}/bin/${PLATFORM}"
+export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9.x/"
+
+export CC=${DROIDTOOLS}-gcc
+export LD=${DROIDTOOLS}-ld
+export CPP=${DROIDTOOLS}-cpp
+export CXX=${DROIDTOOLS}-g++
+export AR=${DROIDTOOLS}-ar
+export AS=${DROIDTOOLS}-as
+export NM=${DROIDTOOLS}-nm
+export STRIP=${DROIDTOOLS}-strip
+export CXXCPP=${DROIDTOOLS}-cppf
+export RANLIB=${DROIDTOOLS}-ranlib
+
+export LDFLAGS="-Os -fPIC -nostdlib -Wl,-rpath-link=${SYSROOT}/usr/lib -L${SYSROOT}/usr/lib -L${DROID_GCC_LIBS} -L${ROOTDIR}/lib"
+export LIBS="-lgcc -lc"
+export CFLAGS="-Os -pipe -isysroot ${SYSROOT} -I${ROOTDIR}/include"
+export CXXFLAGS="-Os -pipe -isysroot ${SYSROOT} -I${ROOTDIR}/include"
+
+# Move to libcurl path and configure
+pushd "${1}"
+./configure --host=${ARCH_TARGET} --target=${PLATFORM} --prefix=${ROOTDIR} --without-zlib --without-ssl --with-random=/dev/urandom --enable-optimize --enable-nonblocking --disable-ares --disable-ftp --disable-ldap --disable-ldaps --disable-rtsp --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp --disable-gopher --disable-sspi --disable-ipv6 --disable-soname-bump --without-polarssl --without-gnutls --without-cyassl --without-axtls --without-libssh2 --disable-manual --disable-verbose
+
+# Fix libtool to not create versioned shared libraries
+mv "libtool" "libtool~"
+sed "s/library_names_spec=\".*\"/library_names_spec=\"~##~libname~##~{shared_ext}\"/" libtool~ > libtool~1
+sed "s/soname_spec=\".*\"/soname_spec=\"~##~{libname}~##~{shared_ext}\"/" libtool~1 > libtool~2
+sed "s/~##~/\\\\$/g" libtool~2 > libtool
+chmod u+x libtool
+
+# Fix curl.h to compile on linux based systems
+mv "include/curl/curl.h" "include/curl/curl.h~"
+sed 's/#include <sys\/types.h>/#include <sys\/select.h>\
+#include <sys\/types.h>/' include/curl/curl.h~ > include/curl/curl.h
+
+# Make, install, and clean
+pushd "lib"
+make
+make install
+popd
+pushd "include"
+make
+make install
+popd
+make clean
+
+# Return to original directory and install shared libraries
+popd
+
+
+
+mkdir -p jni/armeabi/
+cp "${ROOTDIR}/lib/libcurl.so" jni/armeabi/
+
+
+
+
+
+
 ###############################################################################
 # Begin armv7 compile
 
@@ -14,10 +86,10 @@ export TOOLCHAIN_VERSION="4.9"
 
 export ARCH_TARGET="armv7-android-linux"
 export PLATFORM="arm-linux-androideabi"
-export ROOTDIR="/opt/android-ndk-r11c/arm/"
+export ROOTDIR="/opt/android-ndk-r12b/arm/"
 export SYSROOT="${ROOTDIR}/sysroot"
 export DROIDTOOLS="${ROOTDIR}/bin/${PLATFORM}"
-export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9/"
+export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9.x/"
 
 export CC=${DROIDTOOLS}-gcc
 export LD=${DROIDTOOLS}-ld
@@ -75,10 +147,10 @@ cp "${ROOTDIR}/lib/libcurl.so" jni/armeabi-v7a/
 
 export ARCH_TARGET="x86-android-linux"
 export PLATFORM="i686-linux-android"
-export ROOTDIR="/opt/android-ndk-r11c/x86/"
+export ROOTDIR="/opt/android-ndk-r12b/x86/"
 export SYSROOT="${ROOTDIR}/sysroot"
 export DROIDTOOLS="${ROOTDIR}/bin/${PLATFORM}"
-export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9/"
+export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9.x/"
 
 export CC=${DROIDTOOLS}-gcc
 export LD=${DROIDTOOLS}-ld
@@ -138,10 +210,10 @@ cp "${ROOTDIR}/lib/libcurl.so" jni/x86/
 
 export ARCH_TARGET="aarch64-android-linux"
 export PLATFORM="aarch64-linux-android"
-export ROOTDIR="/opt/android-ndk-r11c/arm64/"
+export ROOTDIR="/opt/android-ndk-r12b/arm64/"
 export SYSROOT="${ROOTDIR}/sysroot"
 export DROIDTOOLS="${ROOTDIR}/bin/${PLATFORM}"
-export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9/"
+export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9.x/"
 
 export CC=${DROIDTOOLS}-gcc
 export LD=${DROIDTOOLS}-ld
@@ -205,10 +277,10 @@ cp "${ROOTDIR}/lib/libcurl.so" jni/arm64-v8a/
 
 export ARCH_TARGET="x86_64-android-linux"
 export PLATFORM="x86_64-linux-android"
-export ROOTDIR="/opt/android-ndk-r11c/x86_64/"
+export ROOTDIR="/opt/android-ndk-r12b/x86_64/"
 export SYSROOT="${ROOTDIR}/sysroot"
 export DROIDTOOLS="${ROOTDIR}/bin/${PLATFORM}"
-export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9/"
+export DROID_GCC_LIBS="${ROOTDIR}/lib/gcc/${PLATFORM}/4.9.x/"
 
 export CC=${DROIDTOOLS}-gcc
 export LD=${DROIDTOOLS}-ld
@@ -257,3 +329,5 @@ make clean
 popd
 mkdir -p jni/x86_64/
 cp "${ROOTDIR}/lib/libcurl.so" jni/x86_64/
+
+
